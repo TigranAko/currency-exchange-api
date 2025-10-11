677875584319a1602f73d8ccbabcb40764dcf8f7
Tigran <tigranakopyan2007.ru@gmail.com>
2025-10-11 21:45:18 +0300

    feat(api): add handler get /currency/list to get currency list

--- app/api/endpoints/currency.py
+++ app/api/endpoints/currency.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, Response
 from app.api.schemas.currency import SCurrency
-from app.utils.external_api import get_currency_exchenge
+from app.utils.external_api import get_currency_exchenge, get_currency_list
 
 router = APIRouter(
         prefix="/currency",
@@ -10,6 +10,14 @@ router = APIRouter(
 @router.get("/exchange")
 def get_currency_exchange_handler(response: Response, currency: SCurrency = Depends()):
     result = get_currency_exchenge(currency)
+    response.status_code = result["status_code"]
+    print(result)
+    return result["json"]
+
+
+@router.get("/list")
+def get_currency_list_handler():
+    result = get_currency_list()
     response.status_code = result["status_code"]
     print(result)
     return result["json"]
