d4aac93d5f47ca6e9e82fe0acba9eec1d740419e
Tigran <tigranakopyan2007.ru@gmail.com>
2025-10-13 15:46:16 +0300

    feat(auth): add password hash by bcrypt

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -1,9 +1,8 @@
-
 from fastapi import APIRouter, HTTPException, status, Response, Depends
 #from fastapi.security.http import HTTPBasicCredentials
 from app.api.schemas.user import UserCreds, UserInDB
-from app.core.security import create_jwt_acces_token, create_cookie_auth#, get_username
-from app.core.security import security, get_username
+from app.core.security import create_jwt_acces_token, create_cookie_auth, security, get_username, create_password_hash, verify_password_hash
+
 
 router = APIRouter(
         prefix="/auth",
@@ -11,54 +10,35 @@ router = APIRouter(
         )
 
 
-u = UserInDB(username="test", hashed_password="test")
+u = UserInDB(username="test", hashed_password=create_password_hash("password"))
 
 fake_db: dict[str, str] = {u.username: u.hashed_password}
 print(fake_db)
 
 
-#def verify_user(user: UserInDB):
-#    if user.username != "test" and user.hashed_password != "test":
-#        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-#    return True
-
-
-#def get_user_id(creds: UserCreds):
-#    for user in fake_db.values():
-#        if user.username == creds.username:
-#            return user.user_id
-#    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-
-#def get_user_by_id(user_id):
-#    return fake_db[user_id]
-
-
 @router.post("/register")
 def register(creds: UserCreds, response: Response):
-    user = UserInDB(username=creds.username, hashed_password= creds.password)
 
-
-    # HASHING
-    fake_db[user.username] = user.hashed_password
+    # Нужно добавить проверку на существование в БД
+    password_hash = create_password_hash(creds.password)
+    user = UserInDB(username=creds.username, hashed_password= password_hash)
+    fake_db[user.username] = password_hash
     return "Вы зарегестрированы"
 
 
 @router.post("/login")
 def login(creds: UserCreds, response: Response):
-#    user_id = get_user_id(creds) #
-    if creds.username in fake_db:
-        acces_token = create_jwt_acces_token(creds.username)
-        create_cookie_auth(acces_token, response) # ТУТ ОШИБКА
-        return acces_token
-    else:
+    if not creds.username in fake_db or not verify_password_hash(creds.password, fake_db[creds.username]):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-
+    acces_token = create_jwt_acces_token(creds.username)
+    create_cookie_auth(acces_token, response)
+    return acces_token
 
 
 @router.get("/protected", dependencies=[Depends(security.access_token_required)])
 def protected():
-
     return "You Authorized"
+
 
 @router.get("/me")
 def me(username = Depends(get_username)):
@@ -68,6 +48,7 @@ def me(username = Depends(get_username)):
 @router.get("/logout")
 def logout(response: Response):
     response.delete_cookie("access_token_cookie")
+    #Возможно нужно удалять из security
 
 
 
