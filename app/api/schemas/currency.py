8ab4dbe75f829d6303fcb5893a02b8490a56f5e3
Tigran <tigranakopyan2007.ru@gmail.com>
2025-10-20 10:05:55 +0300

    feature(currency): add error handling

--- app/api/schemas/currency.py
+++ app/api/schemas/currency.py
@@ -1,7 +1,21 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator, Field
+
+from fastapi import HTTPException, status
 
 
 class SCurrency(BaseModel):
-    currency_from: str
-    currency_to: str
+    currency_from: str = Field(max_length=3, min_length=3)
+    currency_to: str = Field(max_length=3, min_length=3)
     amount: float = 1
+
+    @field_validator("currency_from", "currency_to")
+    @classmethod
+    def validate_currency(cls, v):
+        from app.utils.external_api import get_currency_list
+        # когда импорт вверху происходит циклический импорт
+
+        currencies = get_currency_list()["json"]["currencies"].keys()
+        if v.upper() not in currencies:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Выбранный код валюты не найден.")
+        return v
+
