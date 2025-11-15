0a8e35d46de52d6517814c5d0f58669d187c20e0
Tigran <tigranakopyan2007.ru@gmail.com>
2025-11-15 22:08:28 +0300

    style(api): add description for endpoints)

--- app/api/endpoints/currency.py
+++ app/api/endpoints/currency.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, Response
 from app.api.schemas.currency import SCurrency
-from app.utils.external_api import get_currency_exchenge, get_currency_list, update_currency_json, get_currencies_from_json
+from app.utils.external_api import get_currency_exchenge, get_currency_list, get_currencies_from_json
 from app.core.security import security
 
 router = APIRouter(
@@ -8,7 +8,7 @@ router = APIRouter(
         tags=["Currency exchange"]
 )
 
-@router.get("/exchange", dependencies=[Depends(security.access_token_required)])
+@router.get("/exchange", description="Convert currencies", dependencies=[Depends(security.access_token_required)])
 def get_currency_exchange_handler(response: Response, currency: SCurrency = Depends()):
     result = get_currency_exchenge(currency)
     response.status_code = result["status_code"]
@@ -16,7 +16,7 @@ def get_currency_exchange_handler(response: Response, currency: SCurrency = Depe
     return result["json"]
 
 
-@router.get("/list", description="Not recommend", dependencies=[Depends(security.access_token_required)])
+@router.get("/list", description="Get currencies and update currency_list.json", dependencies=[Depends(security.access_token_required)])
 def get_currency_list_handler(response: Response):
     result = get_currency_list()
     response.status_code = result["status_code"]
@@ -24,15 +24,8 @@ def get_currency_list_handler(response: Response):
     return result["json"]
 
 
-@router.get("/update_list_json", dependencies=[Depends(security.access_token_required)])
-def update_currency_file_handler(response: Response):
-    result = update_currency_json()
-    response.status_code = result["status_code"]
-    print(result)
-    return result["json"]
 
-
-@router.get("/list_currency")
+@router.get("/list_currency", description="Get curencise from currency_list.json")
 def get_currency_from_file():
     result = get_currencies_from_json()
     return result
