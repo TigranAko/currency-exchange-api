00ce29f6145ad7f75ede52455e50349e54607633
Tigran <tigranakopyan2007.ru@gmail.com>
2025-11-18 14:22:29 +0300

    test(currency): test function currency exchange

--- tests/test_currency.py
+++ tests/test_currency.py
@@ -2,6 +2,7 @@
 from app.utils import external_api as eapi
 from fastapi import HTTPException
 from requests import Response
+from app.api.schemas.currency import SCurrency
 
 
 import pytest
@@ -55,3 +56,29 @@ def test_make_requests(mocker, res_json):
     mock_get.assert_called_once()
     # mock_get.return_value.raise_for_status.assert_called_once()
 
+
+def test_get_currency_exchange(mocker):
+    res_json = {
+            "date": "2005-01-01",
+            "historical": True,
+            "info": {
+                "quote": 0.51961,
+                "timestamp": 1104623999
+                },
+            "query": {
+                "amount": 10,
+                "from": "USD",
+                "to": "GBP"
+                },
+            "result": 5.1961,
+            "success": True
+    }
+    mock_get = mocker.patch("requests.get")
+    mock_get.return_value.json = (lambda: res_json)
+    mock_get.return_value.status_code = 200
+    currency = SCurrency(**{"currency_to": "GBP", "currency_from": "Usd","amount": 10})
+
+    response = eapi.get_currency_exchenge(currency)
+    assert response["status_code"] == 200
+    assert response["json"]["result"] == 5.1961
+    mock_get.asserr_called_once()
