47a0c06ece996f4c8c54ebd4d30dac69c25b961e
Tigran <tigranakopyan2007.ru@gmail.com>
2025-11-18 19:22:50 +0300

    test(currency): get currencies and update json

--- tests/test_currency.py
+++ tests/test_currency.py
@@ -3,6 +3,7 @@ from app.utils import external_api as eapi
 from fastapi import HTTPException
 from requests import Response
 from app.api.schemas.currency import SCurrency
+from pathlib import Path
 
 
 import pytest
@@ -81,9 +82,34 @@ def test_get_currency_exchange(mocker):
     response = eapi.get_currency_exchenge(currency)
     assert response["status_code"] == 200
     assert response["json"]["result"] == 5.1961
-    mock_get.asserr_called_once()
+    mock_get.assert_called_once()
 
 
 def test_get_currencies_from_json():
     currencies = eapi.get_currencies_from_json()
     assert len(currencies) == 172
+
+
+def test_currency_json(mocker):
+    file_name = "test.json"
+    file_path = Path(file_name)
+    res_json = {
+            "currencies": {
+                "AED": "United Arab Emirates Dirham",
+                "AFN": "Afghan Afghani"
+                }
+            }
+    mock_get = mocker.patch("requests.get")
+    mock_get.return_value.json = (lambda: res_json)
+    mock_get.return_value.status_code = 200
+
+    response = eapi.get_currency_list(file_name=file_name) # тут отправляется запрос и сохраняется в файле
+    mock_get.assert_called_once()
+    assert file_path.exists()
+
+    currencies_from_file = eapi.get_currencies_from_json(file_name) # чтение из файла
+    assert currencies_from_file == res_json["currencies"]
+
+    file_path.unlink()
+    assert not file_path.exists()
+
